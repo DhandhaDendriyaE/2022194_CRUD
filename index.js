8846fb5e37e84af9df843c6a8526cf01619496ef
“DhandhaDendriyaE” <dhanda.dendriya.ft22@mail.umy.ac.id>
2025-10-20 09:49:58 +0700

    memanggil framework express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+});
+
+app.listen(PORT, () => {
+    console.log('')-
+})
